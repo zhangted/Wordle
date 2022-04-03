bb52fde9884c363e78c5b1db9beeefc8971a3ce6
Teddy <tedcbook@gmail.com>
2022-04-03 19:35:51 -0400

    inline first paint css

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const LodashModuleReplacementPlugin = require('lodash-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-
+const HTMLInlineCSSWebpackPlugin = require('html-inline-css-webpack-plugin').default;
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 const robotoAsyncGoogleFontLink = `<link rel="preload" as="style" onload="this.onload=null;this.rel='stylesheet';" href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap"></link>`;
@@ -32,18 +32,6 @@ const config = {
     ]
   },
   plugins: [
-    new HtmlWebpackPlugin({
-      templateContent: ({ htmlWebpackPlugin }) => `
-      <!DOCTYPE html><html>
-      <head>
-      ${robotoAsyncGoogleFontLink}
-      <meta charset=\"utf-8\">
-      <title>Wordle</title><meta name="viewport" content="width=device-width, initial-scale=1.0">
-      </head>
-      <body><div id=\"app\"></div></body></html>`,
-      filename: 'index.html',
-    }),
-    new LodashModuleReplacementPlugin,
     new MiniCssExtractPlugin({
       insert: function(linkTag) {
         linkTag.media="print";
@@ -55,6 +43,24 @@ const config = {
       },
       filename: '[name].[chunkhash].css',
     }),
+    new HtmlWebpackPlugin({
+      templateContent: ({ htmlWebpackPlugin }) => `
+      <!DOCTYPE html><html>
+      <head>
+      ${robotoAsyncGoogleFontLink}
+      <meta charset=\"utf-8\">
+      <title>Wordle</title><meta name="viewport" content="width=device-width, initial-scale=1.0">
+      </head>
+      <body><div id=\"app\"></div></body></html>`,
+      filename: 'index.html',
+    }),
+    new HTMLInlineCSSWebpackPlugin({
+      leaveCSSFile: true,
+      styleTagFactory: ({ style }) => {
+        return `<style class="inline-css">${style}</style>`;
+      }
+    }),
+    new LodashModuleReplacementPlugin,
     new BundleAnalyzerPlugin({
       analyzerMode: 'static',
       openAnalyzer: false,
